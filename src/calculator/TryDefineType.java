677e629388b2c9ae463f5714240dd4d13082da37
Kalamadeo93 <persoonaj@gmail.com>
2021-10-26 23:43:17 +0300

    Commit

---
+++ src/calculator/TryDefineType.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import java.util.Scanner;
+public class TryDefineType {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()){
+
+        }
+
+
+    }
+}
