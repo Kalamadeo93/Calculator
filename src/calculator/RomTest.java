598fbc5fc35fdb3751a58c2bea03fa9fe0de2676
Kalamadeo93 <persoonaj@gmail.com>
2021-10-27 03:47:05 +0300

    Commit

--- src/calculator/RomTest.java
+++ src/calculator/RomTest.java
@@ -1,101 +1,83 @@
 package calculator;
 
-
 import java.util.Scanner;
-
 public class RomTest {
+    static int x;
+    static String y;
+    static int res;
+    static String res1;
 
-    static int last = 2000;
-    static int[] numbers =     {   1,   4,   5,    9,  10,  50, 100, 500, 1000 };
-    static String[] letters =  { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+    private static String arabicToRoman(){
+        switch (x) {
+            case 1:
+                res1 = "I";
+            case 2:
+                res1 = "II";
+            case 3:
+                res1 = "III";
+            case 4:
+                res1 = "IV";
+            case 5:
+                res1 = "V";
+            case 6:
+                res1 = "VI";
+            case 7:
+                res1 = "VII";
+            case 8:
+                res1 = "VIII";
+            case 9:
+                res1 = "IX";
+            case 10:
+                res1 = "X";
+        }
+        return null;
+    }
+    private static int romanToArabic(){
+        switch (y) {
+            case "I":
+                res = 1;
+            case "II":
+                res = 2;
+            case "III":
+                res = 3;
+            case "IV":
+                res = 4;
+            case "V":
+                res = 5;
+            case "VI":
+                res = 6;
+            case "VII":
+                res = 7;
+            case "VIII":
+                res = 8;
+            case "IX":
+                res = 9;
+            case "X":
+                res = 10;
+            default:
+                res = -1;
+
+        }
+        return 0;
+    }
+
+
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
-        //ввод числа
-        System.out.println("Enter your Roman Numerals or Integer number:");
-        Scanner cti = new Scanner(System.in);
-        String a = cti.next();
+        if (sc.hasNextInt())
+        {
+            x = sc.nextInt();
+            System.out.println(arabicToRoman());
 
-        //проверяем входит ли строка "a" в массив "letters"
-        boolean flag = false;
-        for (String letter : letters)
-            if (letter.equals(a)) {
-                flag = true;
-                break;
-            }
-        //объявление числововой переменной
-        int n = Integer.parseInt(a);
-        if (n >= 1) {
-            if (n == 1) {
-                a = "I";
-                System.out.println(a);
-            }
-            else if (n == 2)
+        } else {
+            if (sc.hasNext())
             {
-                a = "II";
-                System.out.println(a);
+                y = sc.nextLine();
+                System.out.println(romanToArabic());
             }
-            else if (n == 3)
-            {
-                a = "III";
-                System.out.println(a);
-            }
-            else if (n==4)
-            {
-                a = "IV";
-                System.out.println(a);
-            }
-            else if (n == 5)
-            {
-                a = "V";
-                System.out.println(a);
-            }
-            else if (n == 6)
-            {
-                a = "VI";
-                System.out.println(a);
-            }
-            else if (n == 7)
-            {
-                a = "VII";
-                System.out.println(a);
-            }
-            else if (n == 8)
-            {
-                a = "VIII";
-                System.out.println(a);
-            }
-            else if (n == 9)
-            {
-                a = "IX";
-                System.out.println(a);
-            }
-            else if (n == 10) {
-                a = "X";
-                System.out.println(a);
-            }
-
         }
 
 
-        //flag = true значит будем иметь дело с римскими обозначением
-        if(flag)
-            System.out.println("Integer value = " +letterToNumber(a) );
-            // тут уже самому можно доделать для перегона в римские по аналогии
-
-    }
-    private static int letterToNumber(String letter){
-        return switch (letter) {
-            case "I" -> 1;
-            case "II" -> 2;
-            case "III" -> 3;
-            case "IV" -> 4;
-            case "V" -> 5;
-            case "VI" -> 6;
-            case "VII" -> 7;
-            case "VIII" -> 8;
-            case "IX" -> 9;
-            case "X" -> 10;
-            default -> -1;
-        };
     }
 }
--- src/calculator/TryDefineType.java
+++ src/calculator/TryDefineType.java
@@ -5,8 +5,16 @@ public class TryDefineType {
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        int x;
+        String y;
         if (sc.hasNextInt()){
-
+            x = sc.nextInt();
+            System.out.println(x);
+        } else {
+            if (sc.hasNext()){
+                y = sc.nextLine();
+                System.out.println(y);
+            }
         }
 
 
