615770bdaccb5ab83a69b4936a30703e80a73ad2
Kalamadeo93 <persoonaj@gmail.com>
2021-11-01 11:47:40 +0300

    Commit

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,20 +1,23 @@
 package calculator;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
 
     public static void main(String[] args) {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        String[] letters = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
         String firstValueRoman = "";
         String secondValueRoman = "";
         int firstValueArabic = 0;
         int secondValueArabic = 0;
         int res = 0;
+
+
         Scanner sc = new Scanner(System.in);
         String all = sc.nextLine();
-
-
         String[] var1 = all.split(" ");
         firstValueArabic = Integer.parseInt(var1[0]);
         secondValueArabic = Integer.parseInt(var1[2]);
@@ -28,5 +31,23 @@ public class Main {
 
         }
         System.out.println(res);
+    }
+    public boolean checkRomanOrArabic(String[] var1){
+        boolean flag;
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        if (var1[0].contains(numbers))
+        {
+
+
+
+        } else {
+            if (sc.hasNext())
+            {
+                y = sc.nextLine();
+            }
+        }
+
+
     }
 }
